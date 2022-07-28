e30bf849d99994cb29d7e5feb86eab6a64e21754
Yash Pratap Singh <singhyashpratap06@gmail.com>
2022-07-29 03:35:30 +0530

    Add imagesSaga to fetch images and show/set errors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import loadingReduceer from './loadingReducer';
 import imagesReducer from './imagesReducer';
 import errorReducer from './errorReducer';
+import pageReducer from './pageReducer';
 
 const rootReducer = combineReducers({
     isLoading: loadingReduceer,
     images: imagesReducer,
     error: errorReducer,
+    nextPage: pageReducer,
 });
 
 export default rootReducer;
---
+++ src/sagas/imagesSaga.js
@@ -0,0 +1,20 @@
+import { IMAGES } from '../constants';
+import { takeEvery, select, call, put } from 'redux-saga/effects';
+import { setImages, setError } from '../actions';
+import { fetchImages } from '../api';
+
+const getPage = state => state.nextPage;
+
+function* handleImagesLoad() {
+    try {
+        const page = yield select(getPage);
+        const images = yield call(fetchImages, page);
+        yield put(setImages(images));
+    } catch (error) {
+        yield put(setError(error.toString()));
+    }
+}
+
+export default function* watchImagesLoad() {
+    yield takeEvery(IMAGES.LOAD, handleImagesLoad);
+}
